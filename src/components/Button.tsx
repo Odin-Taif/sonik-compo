4ed8a6a729f9a569808590fe82ebb4c62b9fddb3
Odin Thif <mjd.thif@icloud.com>
2024-10-28 10:28:07 +0100

    adding a disable property

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
 type Props = {
   label: string;
   color: string;
+  disabled?: boolean;
 };
-export const Button = ({ label, color }: Props) => {
+export const Button = ({ label, disabled, color }: Props) => {
   return (
     <div className={`bg-pink p-2 w-full flex justify-center`}>
       <button
+        disabled={disabled}
         className={`px-10 py-4 rounded-sm text-white hover:shadow-lg ${color}`}
       >
         {label}
